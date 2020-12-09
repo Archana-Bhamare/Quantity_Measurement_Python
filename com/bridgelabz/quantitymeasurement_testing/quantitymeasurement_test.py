78444f5dd0dcb21a11d0a26fabbbdcb0cdf149c4
Archana-Bhamare <archusonar2@gmail.com>
2020-12-09 11:35:12 +0530

    Added : Test case for compare length 36in = 1yd

--- com/bridgelabz/quantitymeasurement_testing/quantitymeasurement_test.py
+++ com/bridgelabz/quantitymeasurement_testing/quantitymeasurement_test.py
@@ -1,7 +1,6 @@
 from com.bridgelabz.quantitymeasurement.Feet import Feet
 from com.bridgelabz.quantitymeasurement.Yard import Yard
 from com.bridgelabz.quantitymeasurement.Inch import Inch
-import pytest
 
 
 # Test_case1 : For compare 3ft = 1yd
@@ -20,6 +19,12 @@ def test_givenOneFeetAndOneYardValue_WhenCompared_ShouldReturnFalse():
 
 # Test_case3 : For compare 1in != 1yd
 def test_givenOneInchAndOneYardValue_WhenCompared_ShouldReturnFalse():
-    first_inch = Feet(1.0)
+    first_inch = Inch(1.0)
     second_yard = Yard(1.0)
     assert first_inch != second_yard
+
+
+def test_givenOneInchAndOneYardValue_WhenCompared_ShouldReturnTrue():
+    first_inch = Inch(36.0)
+    second_yard = Yard(1.0)
+    assert first_inch == second_yard
