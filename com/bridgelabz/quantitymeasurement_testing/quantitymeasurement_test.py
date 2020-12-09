39811dacf01b527f11219eb2276ab9242d05180a
Archana-Bhamare <archusonar2@gmail.com>
2020-12-09 11:01:10 +0530

    Added : Test case for comparing length 3ft = 1yd

--- com/bridgelabz/quantitymeasurement_testing/quantitymeasurement_test.py
+++ com/bridgelabz/quantitymeasurement_testing/quantitymeasurement_test.py
@@ -5,7 +5,10 @@ import pytest
 
 
 # Test_case1 : For compare 3ft = 1yd
-def test_givenTwoYardValue_WhenCompared_ShouldReturnTrue1():
+def test_givenOneFeetAndOneYardValue_WhenCompared_ShouldReturnTrue():
     first_feet = Feet(3.0)
     second_yard = Yard(1.0)
     assert first_feet == second_yard
+
+
+
