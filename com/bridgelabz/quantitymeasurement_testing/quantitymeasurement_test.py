c797f92101258ee3cbe9681a58467e94f9082dc6
Archana-Bhamare <archusonar2@gmail.com>
2020-12-09 10:27:48 +0530

    Added : Test case for comparing two instance feet variable

--- com/bridgelabz/quantitymeasurement_testing/quantitymeasurement_test.py
+++ com/bridgelabz/quantitymeasurement_testing/quantitymeasurement_test.py
@@ -7,3 +7,9 @@ def test_givenTwoFeetValue_WhenCompared_ShouldReturnTrue():
     first_feet = Feet(0.0)
     second_feet = Feet(0.0)
     assert first_feet == second_feet
+
+
+def test_givenTwoFeetValueInstanceVariable_WhenCompared_ShouldReturnTrue():
+    first_feet = Feet(0.0)
+    second_feet = first_feet
+    assert first_feet == second_feet
