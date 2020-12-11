3400ae2dfeb3e66ac27404856052047ea4df1a5f
Archana-Bhamare <archusonar2@gmail.com>
2020-12-11 23:11:20 +0530

    Added : Unit class and QuantityMeasurement class

---
+++ com/bridgelabz/quantitymeasurement/Unit.py
@@ -0,0 +1,17 @@
+import enum
+
+
+class Unit(enum.Enum):
+    FEET = 12.0
+    YARD = 36.0
+    INCH = 1.0
+    CM = 0.4
+    GALLON = 3.78
+    LITRE = 1
+    ML = 0.001
+
+    def __init__(self, unit):
+        self.unit = unit
+
+    def convert(self, length):
+        return self.unit * length
