9c794c0dfa5d28187ad465e34b6f35cbda58b649
Archana-Bhamare <archusonar2@gmail.com>
2020-12-11 22:44:50 +0530

    Added : Add volumes litre, Gallon, Ml

--- com/bridgelabz/quantitymeasurement/QuantityMeasurement.py
+++ com/bridgelabz/quantitymeasurement/QuantityMeasurement.py
@@ -1,4 +1,4 @@
-import enum
+from com.bridgelabz.quantitymeasurement.Unit import *
 
 
 class QuantityMeasurement:
@@ -13,21 +13,8 @@ class QuantityMeasurement:
     def compareto(self, other):
         if isinstance(self.unit, Length) and isinstance(other.unit, Length):
             return Length.convert(self.unit, self.length) == Length.convert(other.unit, other.length)
-
         return False
 
     def addition(self, other):
         return Length.convert(self.unit, self.length) + Length.convert(other.unit, other.length)
 
-
-class Length(enum.Enum):
-    FEET = 12.0
-    YARD = 36.0
-    INCH = 1.0
-    CM = 0.4
-
-    def __init__(self, unit):
-        self.unit = unit
-
-    def convert(self, length):
-        return self.unit * length
