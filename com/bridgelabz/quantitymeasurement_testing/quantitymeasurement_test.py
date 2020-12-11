8435705947ebf14a336a4c9341154016cbf81651
Archana-Bhamare <archusonar2@gmail.com>
2020-12-11 09:49:36 +0530

    Added : Test case-when add two length unit then should return expected

--- com/bridgelabz/quantitymeasurement_testing/quantitymeasurement_test.py
+++ com/bridgelabz/quantitymeasurement_testing/quantitymeasurement_test.py
@@ -27,7 +27,7 @@ def test_givenZeroFtValue_WhenComparedIfNotNone_ShouldReturnTrue():
 def test_givenZeroFeetAndFloatValue_WhenCompared_ShouldReturnTrue():
     first_feet = QuantityMeasurement(Length.FEET, 0.0)
     second_feet = float(0.0)
-    with pytest.raises(AttributeError):
+    with pytest.raises(AssertionError):
         assert first_feet == second_feet
 
 
@@ -59,7 +59,7 @@ def test_givenZeroYardValue_WhenComparedIfNotNone_ShouldReturnTrue():
 def test_givenZeroYardAndFloatValue_WhenCompared_ShouldReturnTrue():
     first_yard = QuantityMeasurement(Length.YARD, 0.0)
     second_yard = float(0.0)
-    with pytest.raises(AttributeError):
+    with pytest.raises(AssertionError):
         assert first_yard == second_yard
 
 
@@ -90,7 +90,7 @@ def test_givenZeroInchValue_WhenComparedIfNotNone_ShouldReturnTrue():
 def test_givenZeroInchAndFloatValue_WhenCompared_ShouldReturnTrue():
     first_inch = QuantityMeasurement(Length.INCH, 0.0)
     second_inch = float(0.0)
-    with pytest.raises(AttributeError):
+    with pytest.raises(AssertionError):
         assert first_inch == second_inch
 
 
@@ -100,56 +100,33 @@ def test_givenZeroInchandOneInch_WhenCompared_ShouldReturnFalse():
     assert first_inch != second_inch
 
 
-# UC2
-# Test_case1 : For compare 3ft = 1yd
-def test_given_3FeetAnd_1YardValue_WhenCompared_ShouldReturnTrue():
-    first_feet = QuantityMeasurement(Length.FEET, 3.0)
-    second_yard = QuantityMeasurement(Length.YARD, 1.0)
-    with pytest.raises(AssertionError):
-        assert first_feet == second_yard
+@pytest.mark.parametrize("first_length, second_length,expected",
+                         [
+                             (QuantityMeasurement(Length.FEET, 3.0), QuantityMeasurement(Length.YARD, 1.0), True),
+                             (QuantityMeasurement(Length.FEET, 1.0), QuantityMeasurement(Length.YARD, 1.0), False),
+                             (QuantityMeasurement(Length.INCH, 1.0), QuantityMeasurement(Length.YARD, 1.0), False),
+                             (QuantityMeasurement(Length.INCH, 36.0), QuantityMeasurement(Length.YARD, 1.0), True),
+                             (QuantityMeasurement(Length.YARD, 1.0), QuantityMeasurement(Length.FEET, 3.0), True),
+                             (QuantityMeasurement(Length.YARD, 1.0), QuantityMeasurement(Length.INCH, 36.0), True)
+                         ])
+def test_givenTwoLengthsUnitValue_WhenCompared_ShoudReturnExpected(first_length, second_length, expected):
+    assert QuantityMeasurement.compareto(first_length, second_length) == expected
 
 
-# Test_case2 : For compare 1ft != 1yd
-def test_given_1FeetAnd_1YardValue_WhenCompared_ShouldReturnFalse():
-    first_feet = QuantityMeasurement(Length.FEET, 1.0)
-    second_yard = QuantityMeasurement(Length.YARD, 1.0)
-    assert first_feet != second_yard
-
-
-# Test_case3 : For compare 1in != 1yd
-def test_givenOneInchAndOneYardValue_WhenCompared_ShouldReturnFalse():
-    first_inch = QuantityMeasurement(Length.INCH, 1.0)
-    second_yard = QuantityMeasurement(Length.YARD, 1.0)
-    assert first_inch != second_yard
-
-
-# Test_case4 : For Compare 36in = 1yd
-def test_given_36InchAndOneYardValue_WhenCompared_ShouldReturnTrue():
-    first_inch = QuantityMeasurement(Length.INCH, 36.0)
-    second_yard = QuantityMeasurement(Length.YARD, 1.0)
-    with pytest.raises(AssertionError):
-        assert first_inch == second_yard
-
-
-# Test_case5 : For compare 1yd = 3ft
-def test_givenOneYardAnd_3FeetValue_WhenCompared_ShouldReturnTrue():
-    first_yard = QuantityMeasurement(Length.YARD, 1.0)
-    second_feet = QuantityMeasurement(Length.FEET, 3.0)
-    with pytest.raises(AssertionError):
-        assert first_yard == second_feet
-
-
-# Test_case6 : For compare 1yd = 36in
-def test_givenOneYardAnd_36InchValue_WhenCompared_ShouldReturnTrue():
-    first_yard = QuantityMeasurement(Length.YARD, 1.0)
-    second_inch = QuantityMeasurement(Length.INCH, 36.0)
-    with pytest.raises(AssertionError):
-        assert first_yard == second_inch
-
-
-# UC3 : 2in = 5cm
-def test_givenTwoInchAndFiveCMValue_WhenCompared_ShouldReturnTrue():
+def test_given_OneInchandOneCM_WhenCompared_ShouldReturnTrue():
     first_inch = QuantityMeasurement(Length.INCH, 2.0)
-    second_cm = QuantityMeasurement(Length.CM, 5.0)
+    second_cm = QuantityMeasurement(Length.CM, 2.5)
     with pytest.raises(AssertionError):
-        assert first_inch == second_cm
+        assert first_inch.compareto(second_cm)
+
+
+@pytest.mark.parametrize("first_length, second_length,expected",
+                         [
+                             (QuantityMeasurement(Length.INCH, 2.0), QuantityMeasurement(Length.INCH, 2.0), 4.0),
+                             (QuantityMeasurement(Length.FEET, 1.0), QuantityMeasurement(Length.INCH, 2.0), 14.0),
+                             (QuantityMeasurement(Length.FEET, 1.0), QuantityMeasurement(Length.FEET, 1.0), 24.0),
+                             (QuantityMeasurement(Length.INCH, 2.0), QuantityMeasurement(Length.CM, 2.5), 3.0),
+
+                         ])
+def test_givenTwoLengthsUnitValue_WhenAdd_ShouldReturnExpectedResult(first_length, second_length, expected):
+    assert QuantityMeasurement.addition(first_length, second_length) == expected
