85aab36f172ae686e4ac262de9b89c11401fe3c4
Archana-Bhamare <archusonar2@gmail.com>
2020-12-12 09:17:08 +0530

    Added : Add QuantityMeasurement class

--- com/bridgelabz/quantitymeasurement/QuantityMeasurement.py
+++ com/bridgelabz/quantitymeasurement/QuantityMeasurement.py
@@ -2,18 +2,38 @@ from com.bridgelabz.quantitymeasurement.Unit import Unit
 
 
 class QuantityMeasurement:
-    def __init__(self, unit, length):
+    def __init__(self, unit, value):
         self.unit = unit
-        self.length = length
+        self.value = value
 
     def __eq__(self, other):
         if isinstance(other, QuantityMeasurement):
-            return self.unit == other.unit and other.length == self.length
+            return self.unit == other.unit and other.value == self.value
 
     def compareto(self, other):
+        """
+
+        Compare Two unit
+        :return: if units are equal then return true otherwise false
+        """
         if isinstance(self.unit, Unit) and isinstance(other.unit, Unit):
-            return Unit.convert(self.unit, self.length) == Unit.convert(other.unit, other.length)
+            return Unit.convert(self.unit, self.value) == Unit.convert(other.unit, other.value)
+        return False
+
+    def compare(self, other):
+        """
+
+        Compare Fanhrehein and Celsius
+        :return: when compare if equal then return true otherwise false
+        """
+        if isinstance(self.unit, Unit) and isinstance(other.unit, Unit):
+            return Unit.converttemp(self.unit, self.value) == Unit.converttemp(other.unit, other.value)
         return False
 
     def addition(self, other):
-        return Unit.convert(self.unit, self.length) + Unit.convert(other.unit, other.length)
+        """
+
+        Add two unit lengths
+        :return: Addition of unit
+        """
+        return Unit.convert(self.unit, self.value) + Unit.convert(other.unit, other.value)
